f1224e34c14211ce5e5770bdcfa1e250da518548
Myeongsoo Kim <mkim754@gatech.edu>
2023-06-11 11:52:46 -0400

    Update setup.sh

--- services/setup.sh
+++ services/setup.sh
@@ -3,13 +3,13 @@ wget https://repo1.maven.org/maven2/org/jacoco/org.jacoco.agent/0.8.7/org.jacoco
 wget https://repo1.maven.org/maven2/org/jacoco/org.jacoco.cli/0.8.7/org.jacoco.cli-0.8.7-nodeps.jar
 
 # Install Evo Bench
-. ./java8.env && cd open-source/emb && mvn clean install -DskipTests && mvn dependency:build-classpath -Dmdep.outputFile=cp.txt
+. ./java8.env && cd emb && mvn clean install -DskipTests && mvn dependency:build-classpath -Dmdep.outputFile=cp.txt
 cd ../..
 
 # Install Genome-Nexus
-. ./java8.env && cd open-source/genome-nexus && mvn clean install -DskipTests
+. ./java8.env && cd genome-nexus && mvn clean install -DskipTests
 cd ../..
 
 # Install YouTube Mock service
-. ./java11.env && cd open-source/youtube && mvn clean install -DskipTests && mvn dependency:build-classpath -Dmdep.outputFile=cp.txt
+. ./java11.env && cd youtube && mvn clean install -DskipTests && mvn dependency:build-classpath -Dmdep.outputFile=cp.txt
 cd ../..
