6d15f6427c8cfb672115dc2f65c0dbb6b26ffb5a
Myeongsoo Kim <mkim754@gatech.edu>
2023-06-13 19:29:50 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,11 @@
+DEFAULT_DIR=$(pwd)
+
+# Add docker repo
+sudo apt-get update
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+
 # Install Common Utilities
 sudo apt-get install -y software-properties-common \
 unzip wget gcc git vim libcurl4-nss-dev tmux mitmproxy
@@ -22,10 +30,10 @@ sudo apt-get install -y dotnet-sdk-6.0
 rm packages-microsoft-prod.deb
 
 # Install RESTler
-. ./venv/bin/activate && cd tools/restler && mkdir restler_bin && python ./build-restler.py --dest_dir ./restler_bin
-cd ../..
+. DEFAULT_DIR/venv/bin/activate && cd DEFAULT_DIR/tools/restler && mkdir restler_bin && python ./build-restler.py --dest_dir ./restler_bin
 
 # Install Schemathesis
+cd DEFAULT_DIR
 . ./venv/bin/activate && pip install requests schemathesis
 
 # Install EvoMaster
@@ -34,20 +42,20 @@ unzip evomaster.jar.zip
 rm evomaster.jar.zip
 
 # Install RESTest
+cd DEFAULT_DIR
 . ./java8.env && cd tools/restest && sh scripts/install_dependencies.sh && mvn clean install -DskipTests
-cd ../..
 
 # Install RestTestGen
+cd DEFAULT_DIR
 . ./java11.env && cd tools/resttestgen && ./gradlew install
-cd ../..
 
 # Install bBOXRT
+cd DEFAULT_DIR
 . ./java11.env && cd tools/bboxrt && mvn clean install -DskipTests
-cd ../..
 
 # Install MoREST
+cd DEFAULT_DIR
 cd tools/morest && pip install -r requirements.txt
-cd ../../..
 
 # Install Docker
 sudo apt-get install -y docker.io
@@ -57,13 +65,13 @@ wget https://repo1.maven.org/maven2/org/jacoco/org.jacoco.agent/0.8.7/org.jacoco
 wget https://repo1.maven.org/maven2/org/jacoco/org.jacoco.cli/0.8.7/org.jacoco.cli-0.8.7-nodeps.jar
 
 # Install Evo Bench
+cd DEFAULT_DIR
 . ./java8.env && cd services/emb && mvn clean install -DskipTests && mvn dependency:build-classpath -Dmdep.outputFile=cp.txt
-cd ../..
 
 # Install Genome-Nexus
+cd DEFAULT_DIR
 . ./java8.env && cd services/genome-nexus && mvn clean install -DskipTests
-cd ../..
 
 # Install YouTube Mock service
+cd DEFAULT_DIR
 . ./java11.env && cd services/youtube && mvn clean install -DskipTests && mvn dependency:build-classpath -Dmdep.outputFile=cp.txt
-cd ../..
